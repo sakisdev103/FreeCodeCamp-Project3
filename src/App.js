24d36a5850d5c78daca228febc92049f02a172f8
Your Name <you@example.com>
2023-04-06 20:52:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { drumArray } from './DrumPads';
 
-function App() {
+const App = () => {
+  const [audioName, setAudioName] = useState('Audio');
+
+  //Managing click event and playing selected audio
+  const handleClick= ({id, desc}) =>{
+    setAudioName(desc);
+    const selectedAudio = document.getElementById(id);
+    selectedAudio.play();
+  }
+
+  //Get key press from keyboard
+  const keyAudio = (e) =>{
+    drumArray.forEach(element => {
+      if(element.keyCode === e.code){
+        handleClick(element);
+        return
+      }
+    });
+  }
+
+  //Play the audio when pressing btn from keyboard
+  useEffect(()=>{
+    window.document.addEventListener('keypress', keyAudio);
+
+    return () => {
+      window.document.removeEventListener('keypress', keyAudio);
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="center">
+        <h1>Drum Pad</h1>
+        <div id="drum-machine">
+          <div id="display">
+            <div className="sub-display">
+              <p>{audioName}</p>
+            </div>
+          </div>
+          <div className="drum-pads-container">
+            {
+              drumArray.map((audio)=>{
+                return(
+                  <div 
+                    className='drum-pad'
+                    id={audio.desc}
+                    key={audio.keyCode}
+                    onClick={()=>handleClick(audio)}
+                  >
+                    <p>{audio.id}</p>
+                    <audio className='clip' src={audio.src} id={audio.id}></audio>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/DrumPads.js
@@ -0,0 +1,56 @@
+export const drumArray = [
+    {
+        desc: 'Audio 1',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        id: 'Q',
+        keyCode: 'KeyQ'
+    },
+    {
+        desc: 'Audio 2',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        id: 'W',
+        keyCode: 'KeyW'
+    },
+    {
+        desc: 'Audio 3',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        id: 'E',
+        keyCode: 'KeyE'
+    },
+    {
+        desc: 'Audio 4',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+        id: 'A',
+        keyCode: 'KeyA'
+    },
+    {
+        desc: 'Audio 5',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        id: 'S',
+        keyCode: 'KeyS'
+    },
+    {
+        desc: 'Audio 6',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        id: 'D',
+        keyCode: 'KeyD'
+    },
+    {
+        desc: 'Audio 7',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        id: 'Z',
+        keyCode: 'KeyZ'
+    },
+    {
+        desc: 'Audio 8',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        id: 'X',
+        keyCode: 'KeyX'
+    },
+    {
+        desc: 'Audio 9',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+        id: 'C',
+        keyCode: 'KeyC'
+    },
+]
